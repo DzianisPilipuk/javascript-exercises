2917207b892a37354a06426e3281d0818ac417a2
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-23 20:35:15 +0100

    Solution of 08 and 09 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num_1, num_2) {
+	let result = num_1 + num_2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(num_1, num_2) {
+	let result = num_1 - num_2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  array.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(element => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = 1;
+	for (let i = 0; i < power; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let index = 0; index < num; index++) {
+    result *= (num - index);
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const converted = string.toLowerCase().match(/[a-zA-Z]/g);
+    half_1 = converted.slice(0, converted.length / -2).reverse();
+    half_2 = converted.slice(converted.length / 2);
+    return(half_1.toString() == half_2.toString());
 };
 
 // Do not edit below this line
